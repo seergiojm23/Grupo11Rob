ec8f872f9fc719cd9685cb495bb3403d25d1c6fb
Tu Nombre <you@example.com>
2024-11-20 17:39:06 +0100

    anadida practica4

---
+++ Practica4/src/CMakeFiles/obs_person_tracker_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/obs_person_tracker_autogen"
+  "obs_person_tracker_autogen/mocs_compilation.cpp"
+  "obs_person_tracker_autogen/timestamp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/obs_person_tracker_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
