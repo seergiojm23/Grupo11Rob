b0a292215783cee25ccb1dfe1d3a48f68252691e
Tu Nombre <you@example.com>
2024-12-05 19:57:30 +0100

    ahi va eso

---
+++ Practica4/tracker/cmake-build-debug/src/CMakeFiles/obs_person_tracker.dir/DependInfo.cmake
@@ -0,0 +1,55 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "src/obs_person_tracker_autogen/timestamp" "custom" "src/obs_person_tracker_autogen/deps"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/Camera360RGB.cpp" "src/CMakeFiles/obs_person_tracker.dir/Camera360RGB.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/Camera360RGB.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/CommonBehavior.cpp" "src/CMakeFiles/obs_person_tracker.dir/CommonBehavior.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/CommonBehavior.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/GenericBase.cpp" "src/CMakeFiles/obs_person_tracker.dir/GenericBase.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/GenericBase.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/Grid2D.cpp" "src/CMakeFiles/obs_person_tracker.dir/Grid2D.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/Grid2D.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/Lidar3D.cpp" "src/CMakeFiles/obs_person_tracker.dir/Lidar3D.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/Lidar3D.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/OmniRobot.cpp" "src/CMakeFiles/obs_person_tracker.dir/OmniRobot.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/OmniRobot.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/VisualElementsPub.cpp" "src/CMakeFiles/obs_person_tracker.dir/VisualElementsPub.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/VisualElementsPub.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/src/commonbehaviorI.cpp" "src/CMakeFiles/obs_person_tracker.dir/commonbehaviorI.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/commonbehaviorI.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/src/genericmonitor.cpp" "src/CMakeFiles/obs_person_tracker.dir/genericmonitor.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/genericmonitor.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/src/genericworker.cpp" "src/CMakeFiles/obs_person_tracker.dir/genericworker.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/genericworker.cpp.o.d"
+  "/home/robocomp/robocomp/classes/abstract_graphic_viewer/abstract_graphic_viewer.cpp" "src/CMakeFiles/obs_person_tracker.dir/home/robocomp/robocomp/classes/abstract_graphic_viewer/abstract_graphic_viewer.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/home/robocomp/robocomp/classes/abstract_graphic_viewer/abstract_graphic_viewer.cpp.o.d"
+  "/home/robocomp/robocomp/classes/grafcetStep/GRAFCETStep.cpp" "src/CMakeFiles/obs_person_tracker.dir/home/robocomp/robocomp/classes/grafcetStep/GRAFCETStep.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/home/robocomp/robocomp/classes/grafcetStep/GRAFCETStep.cpp.o.d"
+  "/home/robocomp/robocomp/classes/qlog/qlog.cpp" "src/CMakeFiles/obs_person_tracker.dir/home/robocomp/robocomp/classes/qlog/qlog.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/home/robocomp/robocomp/classes/qlog/qlog.cpp.o.d"
+  "/home/robocomp/robocomp/classes/rapplication/rapplication.cpp" "src/CMakeFiles/obs_person_tracker.dir/home/robocomp/robocomp/classes/rapplication/rapplication.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/home/robocomp/robocomp/classes/rapplication/rapplication.cpp.o.d"
+  "/home/robocomp/robocomp/classes/sigwatch/sigwatch.cpp" "src/CMakeFiles/obs_person_tracker.dir/home/robocomp/robocomp/classes/sigwatch/sigwatch.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/home/robocomp/robocomp/classes/sigwatch/sigwatch.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/src/main.cpp" "src/CMakeFiles/obs_person_tracker.dir/main.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/main.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/obs_person_tracker_autogen/mocs_compilation.cpp" "src/CMakeFiles/obs_person_tracker.dir/obs_person_tracker_autogen/mocs_compilation.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/obs_person_tracker_autogen/mocs_compilation.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/src/specificmonitor.cpp" "src/CMakeFiles/obs_person_tracker.dir/specificmonitor.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/specificmonitor.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/src/specificworker.cpp" "src/CMakeFiles/obs_person_tracker.dir/specificworker.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/specificworker.cpp.o.d"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/src/visualelementspubI.cpp" "src/CMakeFiles/obs_person_tracker.dir/visualelementspubI.cpp.o" "gcc" "src/CMakeFiles/obs_person_tracker.dir/visualelementspubI.cpp.o.d"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/Camera360RGB.h" "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/Camera360RGB.cpp"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/CommonBehavior.h" "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/CommonBehavior.cpp"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/GenericBase.h" "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/GenericBase.cpp"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/Grid2D.h" "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/Grid2D.cpp"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/Lidar3D.h" "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/Lidar3D.cpp"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/OmniRobot.h" "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/OmniRobot.cpp"
+  "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/VisualElementsPub.h" "/home/robocomp/robocomp/components/Grupo11Rob/Practica4/tracker/cmake-build-debug/src/VisualElementsPub.cpp"
+  )
+
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Practica4/tracker/src/CMakeFiles/ICES__home_usuario_robocomp_components_Grupo11Rob_Practica4_tracker_src.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for ICES__home_usuario_robocomp_components_Grupo11Rob_Practica4_tracker_src.
+# This may be replaced when dependencies are built.
